79ad2a55b69a9ebfc52b894d0c51c1a3860ffbd9
yuichi kawano <yuichi@notespace.jp>
2020-09-02 00:16:30 +0900

    aa

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// sessionStorage
 
 describe('AppController', () => {
   let appController: AppController;
